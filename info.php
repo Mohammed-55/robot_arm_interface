77f6fac46f07388aa20b6d58d2766bc9b85ce428
Mohammed-55 <78411501+Mohammed-55@users.noreply.github.com>
2021-06-28 15:45:46 +0300

    Update info.php

--- info.php
+++ info.php
@@ -7,6 +7,6 @@ $connection = new mysqli("localhost","root","","arm_control");
 	while($row = $result ->fetch_assoc()){
 	
 	        
-	echo "motor1= " .$row["motor1"]. ", motor2= " .$row["motor2"]. ", motor3= " .$row["motor3"] . ", motor4= " .$row["motor4"] . ", motor5= " .$row["motor5"] . ", motor6= " .$row["motor6"] ;
+	echo "motor1= " .$row["motor1"]. ", motor2= " .$row["motor2"]. ", motor3= " .$row["motor3"] . ", motor4= " .$row["motor4"] . ", motor5= " .$row["motor5"] . ", motor6= " .$row["motor6"] . ", play= " .$row["play"];
 }
 ?>
